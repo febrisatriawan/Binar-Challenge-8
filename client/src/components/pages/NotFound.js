1fccbc35303d611424faa936dfa08357adbbf45e
febrisatriawan <fsnproject99@gmail.com>
2022-07-04 22:28:39 +0700

    [febri] add pages notfound

---
+++ client/src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="not-found">
+      <h2 className="display-1">404</h2>
+      <h2 className="display-1">Page Not Found</h2>
+    </div>
+  );
+};
+
+export default NotFound;
