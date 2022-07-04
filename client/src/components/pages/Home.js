2f7470bbad56d1dcea3b63a8945585fbd78d8e78
febrisatriawan <fsnproject99@gmail.com>
2022-07-04 22:25:58 +0700

    [febri] add components home.js

---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  const [users, setUser] = useState([]);
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  const loadUsers = async () => {
+    const result = await axios.get("http://localhost:3003/users");
+    setUser(result.data.reverse());
+  };
+
+  const deleteUser = async (id) => {
+    await axios.delete(`http://localhost:3003/users/${id}`);
+    loadUsers();
+  };
+
+  return (
+    <div className="container">
+      <div className="py-4 text-center">
+        <h2>USER PLAYERS</h2>
+        <table class="table border shadow">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Username</th>
+              <th scope="col">Email</th>
+              <th scope="col">Password</th>
+              <th scope="col">Experience</th>
+              <th scope="col">Level</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr>
+                <th scope="row">{index + 1}</th>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>{user.password}</td>
+                <td>{user.experience}</td>
+                <td>{user.lvl}</td>
+                <td>
+                  <Link class="btn btn-primary mr-2" to={`/users/${user.id}`}>
+                    View
+                  </Link>
+                  <Link
+                    class="btn btn-outline-primary mr-2"
+                    to={`/users/edit/${user.id}`}
+                  >
+                    Edit
+                  </Link>
+                  <Link
+                    class="btn btn-danger"
+                    onClick={() => deleteUser(user.id)}
+                  >
+                    Delete
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
