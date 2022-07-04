77bf7dfb750c5a3f5e9fef83f6facb2f6e6188a4
febrisatriawan <fsnproject99@gmail.com>
2022-07-04 22:30:19 +0700

    [febri] add users adduser

---
+++ client/src/components/users/AddUser.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+
+const AddUser = () => {
+  let history = useHistory();
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+    experience: "",
+    lvl: "",
+  });
+
+  const { username, email, password, experience, lvl } = user;
+  const onInputChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:3003/users", user);
+    history.push("/");
+  };
+  return (
+    <div className="container">
+      <div className="w-75 mx-auto shadow p-5">
+        <h2 className="text-center mb-4">Add A User Players</h2>
+        <form onSubmit={(e) => onSubmit(e)}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Username"
+              name="username"
+              value={username}
+              onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control form-control-lg"
+              placeholder="Email"
+              name="email"
+              value={email}
+              onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Experience"
+              name="experience"
+              value={experience}
+              onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="number"
+              className="form-control form-control-lg"
+              placeholder="Level"
+              name="lvl"
+              value={lvl}
+              onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <button className="btn btn-primary btn-block">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddUser;
